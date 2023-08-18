a17102083e6a88641b1a20491cebaa7b792e3fe8
broadfeet110 <broadfeetsales@gmail.com>
2023-08-18 18:04:42 -0400

    Created SpeedTier Page Created a component to fetch pokeAPI data and display in the page.

--- src/components/SpeedTier.js
+++ src/components/SpeedTier.js
@@ -1,20 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 export default function SpeedTier() {
     const [pokeData, setPokeData] = useState('')
-    function getPokemonData() {
-        /* get data from maybe pokeAPI */
-        /* sort pokemon data by speed tiers */
-    }
+    const Pokedex = require('pokeapi-js-wrapper')
+    const P = new Pokedex.Pokedex()
+    const pokemonInSV = [
+        "miraidon"
+    ]
+
+    const getPokemonInfo = useCallback(async () => {
+        let res = await P.getPokemonByName(pokemonInSV[0])
+        console.log(res)
+        setPokeData(res)
+    }, [])
 
     useEffect(() => {
-        setPokeData(getPokemonData())
-    }, [])
+        getPokemonInfo()
+    }, [getPokemonInfo])
 
     return (
         <div id="SpeedTierDiv">
             <h1>Welcome to the Pokemon Battle Companion!</h1>
-            {/* for each speed tier in pokeData, get a row*/}
+
+            {pokeData ?
+                <div>
+                    <img src = { pokeData.sprites.front_default }></img>
+                    <h1 style={{textTransform: "uppercase"}}>{ pokeData.name } </h1>
+                    { pokeData.stats[5].base_stat }
+                
+                </div>
+                : ""
+            }
+            
+            
+            
         </div>
     )
 }
