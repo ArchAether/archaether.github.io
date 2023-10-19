5a687f3f3ba78a355ebd8cb051f9fc99d88083e9
broadfeet110 <broadfeetsales@gmail.com>
2023-10-19 12:55:57 -0400

    Remove older code
    Removed older code that used PokeAPI to get information

--- src/components/SpeedTier.js
+++ src/components/SpeedTier.js
@@ -4,10 +4,6 @@ import { regEList } from './RegEList.js'
 
 export default function SpeedTier() {
     const [pokeData, setPokeData] = useState('')
-    const Pokedex = require('pokeapi-js-wrapper')
-    const P = new Pokedex.Pokedex()
-    
-    const rawPkmnString = "ariados yanmega quagsire mightyena volbeat illumise crawdaunt leavanny ribombee arbok pikachu raichu victreebel furret staraptor lurantis flapple appletun dipplin ninetales poliwrath politoed gyarados noctowl ambipom heracross mamoswine stantler shiftry gardevoir gallade kricketune pachirisu lucario lilligant trevenant lycanroc-midday lycanroc-dusk lycanroc-midnight greedent toedscruel sinistcha arcanine golem sudowoodo conkeldurr noivern barraskewda hatterene morpeko-full-belly orthworm maushold annihilape snorlax ludicolo probopass luxray vikavolt oricorio-baile sandslash gengar gliscor houndoom grumpig mandibuzz mudsdale kommo-o bombirdier weezing mienshao dusclops dusknoir chimecho magcargo chandelure masquerain clefairy clefable bronzong glimmora milotic dudunsparce whiscash garchomp carbink salazzle weavile glalie froslass eelektross goodra swanna drednaw cramorant bisharp kingambit mimikyu-disguised grimmsnarl indeedee-female basculegion-male ursaluna okidogi munkidori fezandipiti ogerpon"
 
     function getRegEPkmn(pokedex, pokeList){
         let reg_e_data = []
@@ -19,24 +15,6 @@ export default function SpeedTier() {
         return reg_e_data
     }
 
-    const getPokemonInfo = useCallback(async () => {
-        let pokeGroup = []
-        
-        let pkmnNames = rawPkmnString.split(" ")
-        for (let i = 0; i < pkmnNames.length; i++) {
-            pokeGroup.push(await P.getPokemonByName(pkmnNames[i]))
-        }
-        pokeGroup.sort((a, b) => b.stats[5].base_stat - a.stats[5].base_stat )
-
-        // const groupByCat = pokeGroup.reduce((group, pokemon) => {
-        //     const base_stat = pokemon.stats[5].base_stat
-        //     group[base_stat] = group[base_stat] ?? []
-        //     group[base_stat].push(pokemon)
-        //     return group
-        // }, {})
-        setPokeData(pokeGroup)
-    }, [])
-
     useEffect(() => {
         setPokeData(getRegEPkmn(pokedex, regEList))
     }, [])
@@ -46,15 +24,6 @@ export default function SpeedTier() {
     }
 
     function PkmnCard (pokemon) {
-        return (
-            <div className="pkmnCard" id={pokemon.name}>
-                <img src={pokemon.sprites.front_default}></img>
-                <h3 style={{textTransform: "uppercase"}}>{pokemon.stats[5].base_stat} { pokemon.name }</h3>
-            </div>
-        )
-    }
-
-    function PkmnCardV2 (pokemon) {
         return (
             <div className="pkmnCard" id={pokemon.name}>
                 <img src={pokemon.sprite}></img>
@@ -68,7 +37,7 @@ export default function SpeedTier() {
             <h1>Welcome to the Pokemon Battle Companion!</h1>
 
             {pokeData ?
-                pokeData.map((pokemon) => <PkmnCardV2 {...pokemon} />)
+                pokeData.map((pokemon) => <PkmnCard {...pokemon} />)
                 : ""
             } 
 
